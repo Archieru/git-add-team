01948d4474de59dc7f4cfa9b6b28232d27ad77a7
Dmitry Pikulkin <dmitry.pikulkin@toptal.com>
2019-05-27 18:53:47 +0300

    Code review results

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ const team = process.env.GIT_GROUP_TO_ADD || 'viewers'
 const access = process.env.GIT_PERMISSION_TO_ADD || 'pull' // pull, push, admin
 
 const fetch = require('node-fetch');
-async function gitFetch(url, params = {}) { return fetch('https://api.github.com/'+url, {...{headers:{'Authorization': 'token '+apiKey}}, ...params}) }
-async function gitOrgFetch(url, params = {}) { return gitFetch('orgs/'+organization+'/'+url, params) }
+function gitFetch(url, params = {}) { return fetch(`https://api.github.com/${url}`, {headers:{'Authorization': 'token '+apiKey}, ...params}) }
+function gitOrgFetch(url) { return gitFetch(`orgs/${organization}/${url}`) }
 
 async function gitAddTeam() {
-    const teamId = await gitOrgFetch('teams/' + team).then(res => res.json()).then(team => team.id)
+    const teamId = await gitOrgFetch(`teams/${team}`).then(res => res.json()).then(team => team.id)
     if (!teamId) { throw new Error('Unable to find team: ' + team)}
         
-    const pagesTotal = await gitOrgFetch('repos').then(res => res.headers.get('link').split(',')[1].match(/page=([0-9]*)>/)[1])
+    const pagesTotal = await gitOrgFetch(`repos`).then(res => res.headers.get('link').split(',')[1].match(/page=([0-9]*)>/)[1])
     
     for (var pageNumber = 1; pageNumber <= pagesTotal; pageNumber++) {
-        gitOrgFetch('repos?page=' + pageNumber).then(res => res.json()).then(json => json.forEach(repo => {
+        gitOrgFetch(`repos?page=${pageNumber}`).then(res => res.json()).then(json => json.forEach(repo => {
             gitFetch(
-                'teams/' + teamId + '/repos/' + organization + '/' + repo.name, 
+                `teams/${teamId}/repos/${organization}/${repo.name}`, 
                 { method:'PUT', body: JSON.stringify({'permission' : access})})
             .then(res => { 
-                if (res.status == 204) { console.log('Successfully added repo: ' + repo.name) }
-                else if (res.status == 403) { console.log('You don\'t have permission to add: ' + repo.name) }
-                else { console.log('General error occured with repo: ' + repo.name + '  Status: ' + res.status) }
+                if (res.status == 204) { console.log(`Successfully added repo: ${repo.name}`) }
+                else if (res.status == 403) { console.log(`You don't have permission to add: ${repo.name}`) }
+                else { console.log(`General error occured with repo: ${repo.name} Status: ${res.status}`) }
             })
         }))
     }
